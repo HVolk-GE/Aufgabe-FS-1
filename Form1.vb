597b2594931db3ab91be7bf5d056cbdc4b1196cb
Horst Volk <horst.volk@t-online.de>
2023-09-02 17:40:28 +0200

    Including ForNext Loop for example don't use on GUI!

--- Form1.vb
+++ Form1.vb
@@ -11,26 +11,33 @@
 
 Public Class Form1
     '* ===================================== Globale Variablen definition ====================================================================
-    Dim byteCounter, byteInkremtint, byteDekremtint As Byte
-    Dim boolDekrem, boolPrgDone As Boolean
+    Dim intCounter, intInkremtint, intDekremtint As Integer
+    Dim boolDekrem, boolPrgDone, boolForNextLoop As Boolean
 
     '* ======================================= Timer definition ==============================================================================
     Private WithEvents oTimerProgress, oTimerCounter As Timer
 
-
     '* ======================================= On Form load actions ==========================================================================
     Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
         Me.NumericUpDown1.Enabled = False
+        '==================================== For Next Switch ================================================================================
+        'Interne Loops sind fuer die Darstellung auf einer GUI nicht geeignet, Pause oder Break werden in neuen Programmiersprachen nicht mehr
+        'benutzt. Da man heute mit Timern arbeiten "soll"!
+        'Auf True setzen, wenn man mit der For Next loop arbeiten moechte.
+        'Bei False wird der oTimerCounter benutzt. Dieses ist besser fuer die Nachvollziehbarkeit (Sichbarkeit) im GUI geeignet!
+        boolForNextLoop = False
     End Sub
 
     '* ==================================== Button Inkrementieren ============================================================================
     Private Sub BtnInkrement_MouseDown(ByVal sender As Object,
     ByVal e As System.Windows.Forms.MouseEventArgs) Handles BtnInkrement.MouseDown
         If boolPrgDone = True Then
-            oTimerCounter.Stop()
+            If boolForNextLoop = False Then
+                oTimerCounter.Stop()
+            End If
             Me.ProgressBar.Value = 0
             Me.NumericUpDown1.Value = 0
-            byteCounter = 0
+            intCounter = 0
             boolPrgDone = False
         End If
         'Dekrementiert deaktiveren und Progressbar fuellen aufrufen
@@ -48,15 +55,17 @@ Public Class Form1
     Private Sub BtnDekrement_MouseDown(ByVal sender As Object,
     ByVal e As System.Windows.Forms.MouseEventArgs) Handles BtnDekrement.MouseDown
         If boolPrgDone = True Then
-            oTimerCounter.Stop()
+            If boolForNextLoop = False Then
+                oTimerCounter.Stop()
+            End If
             Me.ProgressBar.Value = 0
             Me.NumericUpDown1.Value = 0
-            byteCounter = 0
+            intCounter = 0
             boolPrgDone = False
         End If
         'Dekrementiert aktiveren, dekrementier Startwert auf 100 setzen und Progressbar fuellen aufrufen
         boolDekrem = True
-        byteDekremtint = 100
+        intDekremtint = 100
         ProgressbarFill()
     End Sub
 
@@ -86,38 +95,67 @@ Public Class Form1
     '* ==================================== (Timer) Progressbar füllen =========================================================================
     Private Sub oTimerProgress_Tick(ByVal sender As Object,
     ByVal e As System.EventArgs) Handles oTimerProgress.Tick
-        byteCounter += 1
+        intCounter += 1
         'Progressbar füllen auf 100 prozent
         If Me.ProgressBar.Value < 100 Then
-            Me.ProgressBar.Value = byteCounter
+            Me.ProgressBar.Value = intCounter
         Else
             'Ist Progressbar voll, wird dieser Timer gestopt und In-/Dekrementierung gestartet
             oTimerProgress.Stop()
-            InkremDekrem()
+            If boolForNextLoop = False Then
+                InkremDekrem()
+            ElseIf boolForNextLoop = True Then
+                inkrementDekrement() ' Startet For Next Loop, Kalkulationen sind auf der GUI jedoch nicht nachvollziehbar!
+            End If
         End If
+
     End Sub
 
     '* ==================================== (Timer) In-/Dekrementieren =========================================================================
     Private Sub oTimerCounter_Tick(ByVal sender As Object,
     ByVal e As System.EventArgs) Handles oTimerCounter.Tick
+
+        Me.BtnInkrement.Enabled = False
+        Me.BtnDekrement.Enabled = False
         'Zaehler inkrementieren:
-        If byteInkremtint < 100 And boolDekrem = False Then
-            Me.BtnInkrement.Enabled = False
-            Me.BtnDekrement.Enabled = False
-            byteInkremtint += 1
-            Me.NumericUpDown1.Value = byteInkremtint
+        If intInkremtint < 100 And boolDekrem = False Then
+            intInkremtint += 1
+            Me.NumericUpDown1.Value = intInkremtint
             'Zaehler dekrementieren:
-        ElseIf byteDekremtint > 0 And boolDekrem = True Then
-            Me.BtnInkrement.Enabled = False
-            Me.BtnDekrement.Enabled = False
-            byteDekremtint -= 1
-            Me.NumericUpDown1.Value = byteDekremtint
+        ElseIf intDekremtint > 0 And boolDekrem = True Then
+            intDekremtint -= 1
+            Me.NumericUpDown1.Value = intDekremtint
         Else
             'Programm fuer reset, um neu start zu ermoeglichen.
             boolPrgDone = True
             Me.BtnInkrement.Enabled = True
             Me.BtnDekrement.Enabled = True
         End If
+
+    End Sub
+
+    'For Next (Man koennte auch Do While oder For Each nutzen - alle haben jedoch das selbe Problem ->) Loesung,
+    'nicht benutzbar fuer GUI Darstellung, da die Zaehlung schneller ist als die Refreshrate der Monitore!
+    Private Sub inkrementDekrement()
+        Me.BtnInkrement.Enabled = False
+        Me.BtnDekrement.Enabled = False
+        'Zaehler inkrementieren:
+        If intInkremtint < 100 And boolDekrem = False Then
+            For byteInkremtint = 0 To 100 Step 1
+                Me.NumericUpDown1.Value = byteInkremtint
+            Next
+            'Zaehler dekrementieren:
+        ElseIf intDekremtint > 0 And boolDekrem = True Then
+            'For indexB = 20 To 1 Step -3
+            For intDekremtint = 100 To 0 Step -1
+                Me.NumericUpDown1.Value = intDekremtint
+            Next
+        End If
+        'Programm fuer reset, um neu start zu ermoeglichen.
+        boolPrgDone = True
+        Me.BtnInkrement.Enabled = True
+        Me.BtnDekrement.Enabled = True
+
     End Sub
 
 End Class
