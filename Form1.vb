158e386f78162f408d7b71ce46f1f31bdf8022f2
Horst Volk <horst.volk@t-online.de>
2023-09-05 20:57:38 +0200

    Latest Update respo

--- Form1.vb
+++ Form1.vb
@@ -8,36 +8,31 @@
 '* Wenn der 1. Knopf gedrückt und gehalten wird füllt sich der Laufbalken. Ist der Laufbalken voll wird der Zähler inkrementiert
 '* Wenn der 2. Knopf gedrückt und gehalten wird füllt sich der Laufbalken. Ist der Laufbalken voll wird der Zähler dekrementiert
 '* ==========================================================================================================================================
-
+'* ==========================================================================================================================================
+' Anmerkungen:
+' Ein Timer wird in den eingestellten Intervallen das Tick-Event auslösen. Umstellung, also von Sequentiell nach Eventbasiert.
+' Vorteil ist, dass zwischen dem Starten des Timers und dem Tick-Event man auch irgendwas anderes machen kann und der Thread nicht mehr mit
+' einer Do-Schleife beansprucht wird (wie zbsp. bei Sleep, Wait oder Pause es der Fall waere).
+'* ==========================================================================================================================================
 Public Class Form1
     '* ===================================== Globale Variablen definition ====================================================================
     Public intCounter, intInkremtint, intDekremtint As Integer
-    Public boolDekrem, boolPrgDone, boolForNextLoop As Boolean
-    ReadOnly indekrem As New indekrement()
+    Public boolDekrem, boolPrgDone As Boolean
     '* ======================================= Timer definition ==============================================================================
     Private WithEvents oTimerProgress, oTimerCounter As Timer
-
     '* ======================================= On Form load actions ==========================================================================
     Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
         'NumericUpDown1 Feld deaktiviert - wird nur von dem Programm als Ziel der Kalkulationen benutzt!
         Me.NumericUpDown1.Enabled = False
-        '==================================== For Next Switch ================================================================================
-        'Interne Loops sind fuer die Darstellung auf einer GUI nicht geeignet, Pause, Sleep oder Break werden in VB.Net nicht mehr
-        'benutzt. Da man heute mit Timern arbeiten "soll"!
-        'Auf True setzen, wenn man mit der For Next loop arbeiten moechte.
-        'Bei False wird der oTimerCounter benutzt. Dieses ist besser fuer die Nachvollziehbarkeit (Sichbarkeit) im GUI geeignet!
-        boolForNextLoop = False
     End Sub
-
     '* ==================================== Button Inkrementieren ============================================================================
     Private Sub BtnInkrement_MouseDown(ByVal sender As Object,
     ByVal e As System.Windows.Forms.MouseEventArgs) Handles BtnInkrement.MouseDown
+
         'Solange linke Maustaste auf dem Button "Inkrement" gedrückt wird, wird folgender Code ausgeführt:
         'Wenn Programm erfolgreích druchlaufen ist, wird der oTimerCounter gestoppt.
         If boolPrgDone = True Then
-            If boolForNextLoop = False Then
-                oTimerCounter.Stop()
-            End If
+            oTimerCounter.Stop()
             'Neustart Initialisierung
             Me.ProgressBar.Value = 0
             Me.NumericUpDown1.Value = 0
@@ -49,23 +44,23 @@ Public Class Form1
         'Progressbar fuellen aufrufen
         ProgressbarFill()
     End Sub
+    '* ==================================== Button loslassen waerend Progressbar fuellt ======================================================
     Private Sub BtnInkrement_MouseUp(ByVal sender As Object,
     ByVal e As System.Windows.Forms.MouseEventArgs) Handles BtnInkrement.MouseUp
+
         'Dekrementierung deaktiveren = Inkrementierung aktiviert.
         boolDekrem = False
         'Progressbar-Timer stoppen
         oTimerProgress.Stop()
     End Sub
-
     '* ==================================== Button Dekrementieren ============================================================================
     Private Sub BtnDekrement_MouseDown(ByVal sender As Object,
     ByVal e As System.Windows.Forms.MouseEventArgs) Handles BtnDekrement.MouseDown
+
         'Solange linke Maustaste auf dem Button "Dekrement" gedrückt wird, wird folgender Code ausgeführt:
         'Wenn Programm erfolgreích druchlaufen ist, wird der oTimerCounter gestoppt.
         If boolPrgDone = True Then
-            If boolForNextLoop = False Then
-                oTimerCounter.Stop()
-            End If
+            oTimerCounter.Stop()
             'Neustart Initialisierung
             Me.ProgressBar.Value = 0
             Me.NumericUpDown1.Value = 0
@@ -77,18 +72,18 @@ Public Class Form1
         intDekremtint = 100
         ProgressbarFill()
     End Sub
-
+    '* ==================================== Button loslassen waerend Progressbar fuellt ======================================================
     Private Sub BtnDekrement_MouseUp(ByVal sender As Object,
     ByVal e As System.Windows.Forms.MouseEventArgs) Handles BtnDekrement.MouseUp
+
         'Dekrementierung aktiveren = Inkrementierung deaktivert.
         boolDekrem = True
         'Progressbar-Timer stoppen
         oTimerProgress.Stop()
     End Sub
-
     '* ==================================== (Timer) Progressbar füllen starten =================================================================
     Private Sub ProgressbarFill()
-        'Progressbar Timer starten, ticks 100ms
+        'Progessbar Timer starten, ticks(Interval) 100ms
         If IsNothing(oTimerProgress) Then oTimerProgress = New Timer
         oTimerProgress.Interval = 100
         oTimerProgress.Start()
@@ -96,7 +91,7 @@ Public Class Form1
 
     '* ==================================== (Timer) In-/Dekrementieren starten =================================================================
     Private Sub InkremDekrem()
-        'Zaehler Timer 0-100 starten, ticks 100ms
+        'Zaehler Timer starten, ticks(Interval) 100ms
         If IsNothing(oTimerCounter) Then oTimerCounter = New Timer
         oTimerCounter.Interval = 100
         oTimerCounter.Start()
@@ -106,6 +101,7 @@ Public Class Form1
     '* ==================================== (Timer) Progressbar füllen =========================================================================
     Private Sub oTimerProgress_Tick(ByVal sender As Object,
     ByVal e As System.EventArgs) Handles oTimerProgress.Tick
+
         intCounter += 1
         'Progressbar füllen auf 100 prozent
         If Me.ProgressBar.Value < 100 Then
@@ -113,17 +109,10 @@ Public Class Form1
         Else
             'Ist Progressbar voll, wird dieser Timer gestopt und die In-/Dekrementierung gestartet
             oTimerProgress.Stop()
-            'For Next Loop, wenn true dann aktiv, ist nur zu Beispiel zwecken,
-            'da die Werte sich schneller aktualliseren als dies ein Monitor darstellen kann.
-            If boolForNextLoop = False Then
-                InkremDekrem()
-            ElseIf boolForNextLoop = True Then
-                indekrem.inkrementDekrement() 'Startet Klasse in-/dekrementieren!
-            End If
+            InkremDekrem()
         End If
 
     End Sub
-
     '* ==================================== (Timer) In-/Dekrementieren =========================================================================
     Private Sub oTimerCounter_Tick(ByVal sender As Object,
     ByVal e As System.EventArgs) Handles oTimerCounter.Tick
@@ -144,7 +133,5 @@ Public Class Form1
             Me.BtnInkrement.Enabled = True
             Me.BtnDekrement.Enabled = True
         End If
-
     End Sub
-
 End Class
